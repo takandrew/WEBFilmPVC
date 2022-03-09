d91a770f139fdd598ba0a81dd92ce7c8e1d424db
Takandrew <andreikaril@mail.ru>
2022-03-09 17:40:07 +0300

    trying to make some actions

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -1,8 +1,12 @@
-function save () {
+function Show_uploaded_image () {
     let file = photo_pvc.files[0];
     if (file) {
         image_pvc.src = URL.createObjectURL(file);
         localStorage.setItem('myImage', image_pvc.src);
     }
 }
-image_pvc.src = localStorage.setItem('myImage');
+//image_pvc.src = localStorage.setItem('myImage');
+
+const Show_div_image = () => {
+    div_image.classList.toggle("active");
+}
