981e96a7d33ace95545c5a1293cf5969a560a5b3
takandrew <andreikaril@mail.ru>
2022-03-22 13:51:14 +0300

    Fixed previous problem

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -12,8 +12,7 @@ const Show_div_image = () => {
     image.classList.toggle("active", true);
 }
 
-function make_base()
-{
+function make_base() {
     Show_uploaded_image();
     let image = document.getElementById('image-pvc');
     image.onload = function(){
@@ -23,64 +22,90 @@ function make_base()
             needed_width = window.innerWidth - 100;
             needed_height = needed_width/needed_proportion;
         }
-        context.canvas.width = needed_width;
-        context.canvas.height = needed_height;
-        context.drawImage(image, 0, 0, needed_width, needed_height);
+        context_pvc.canvas.width = needed_width;
+        context_pvc.canvas.height = needed_height;
+        context_pvc.drawImage(image, 0, 0, needed_width, needed_height)
+        context_rect.canvas.width = needed_width;
+        context_rect.canvas.height = needed_height;
     }
     Show_div_image();
 }
 
+var canvas_pvc = document.getElementById('canvas-pvc'),
+    context_pvc = canvas_pvc.getContext('2d');
 
-/*TODO: Сделать красивое рисование прямоугольников*/
-/*FIXME: Наладить работу добавления и удаления обработчиков событий; Разобраться в том, что тут происходит не так...*/
-
-var canvas = document.getElementById('canvas-pvc'),
-    context = canvas.getContext('2d');
+var canvas_rect = document.getElementById('canvas-rect'),
+    context_rect = canvas_rect.getContext('2d');
 
 var x_left, x_right, y_left, y_right;
 var draw = false;
 
-function Start_drawing() {
-    canvas.addEventListener("mousedown", mouse_downed(MouseEvent), true);
-    canvas.addEventListener("mousemove", mouse_mooved(MouseEvent), true);
-    canvas.addEventListener("mouseup", mouse_upped(MouseEvent), true);
+function start_drawing() {
+    canvas_rect.style.visibility = "visible";
+    context_rect.clearRect(0,0, canvas_rect.width, canvas_rect.height);
+    canvas_rect.addEventListener("mousedown", mouse_downed, true);
+    canvas_rect.addEventListener("mousemove", mouse_moved, true);
+    canvas_rect.addEventListener("mouseup", mouse_upped, true);
 }
 
-function  Stop_drawing() {
-    canvas.removeEventListener("mouseup", mouse_upped(MouseEvent), true);
-    canvas.removeEventListener("mousedown", mouse_downed(MouseEvent), true);
-    canvas.removeEventListener("mousemove", mouse_mooved(MouseEvent), true);
+function  stop_drawing() {
+    canvas_rect.removeEventListener("mouseup", mouse_upped, true);
+    canvas_rect.removeEventListener("mousedown", mouse_downed, true);
+    canvas_rect.removeEventListener("mousemove", mouse_moved, true);
+    canvas_rect.style.visibility = "hidden";
 }
 
-function mouse_downed(event) {
-    if (event.target !== undefined) {
-        let rect = event.target.getBoundingClientRect();
-        x_left = event.clientX - rect.left;
-        y_left = event.clientY - rect.top;
-        draw = true;
+var is_final_rec = false;
+
+function mouse_downed(e) {
+    is_final_rec = false;
+    let rect = e.target.getBoundingClientRect();
+    x_left = e.clientX - rect.left;
+    y_left = e.clientY - rect.top;
+    draw = true;
+}
+
+function  mouse_moved(e) {
+    if(draw===true){
+        is_final_rec = false;
+        draw_rectangle(e);
     }
 }
 
-function  mouse_mooved(event) {
-    if (event.target !== undefined) {
-        if(draw===true){
-            draw_rectangle(event);
-        }
+function mouse_upped(e) {
+    is_final_rec = true;
+    draw_rectangle(e);
+    draw = false;
+}
+
+class rect_class {
+    constructor(xl, yl, xr, yr) {
+        this.xl = xl;
+        this.yl = yl;
+        this.xr = xr;
+        this.yr = yr;
     }
 }
 
-function mouse_upped(event) {
-    if (event.target !== undefined) {
-        draw_rectangle(event);
-        draw = false;
-    }
-}
+var rect_arr = [];
 
-function draw_rectangle(event) {
-    let rect = event.target.getBoundingClientRect();
-    x_right = event.clientX - rect.left;
-    y_right = event.clientY - rect.top;
+function draw_rectangle(e) {
+    if (!is_final_rec) {
+        context_rect.clearRect(x_left, y_left, Math.abs(x_right-x_left), Math.abs(y_left-y_right));
+    }
+    let rect = e.target.getBoundingClientRect();
+    x_right = e.clientX - rect.left;
+    y_right = e.clientY - rect.top;
     let rect_w = Math.abs(x_right-x_left);
     let rect_h = Math.abs(y_left-y_right);
-    context.strokeRect(x_left,y_left,rect_w,rect_h);
+    if (!is_final_rec) {
+        context_rect.strokeRect(x_left,y_left,rect_w,rect_h);
+    }
+    else {
+        context_rect.strokeRect(x_left,y_left,rect_w,rect_h);
+        context_pvc.strokeRect(x_left,y_left,rect_w,rect_h);
+        let rect_class_temp = new rect_class(x_left,y_left,x_right,y_right);
+        rect_arr.push(rect_class_temp);
+    }
 }
+
